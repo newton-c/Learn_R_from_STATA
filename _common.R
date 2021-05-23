4cbd02f12718e8b5a8baa55edf498805d1f0db31
newton-c <cnewton2@buffalo.edu>
2021-05-23 11:27:55 -0400

    adding formating

---
+++ _common.R
@@ -0,0 +1,13 @@
+set.seed(12582)
+
+knitr::opts_chunk$set(
+                      comment = "#>",
+                      collapse = T,
+                      out.width = "70%",
+                      fig.align = "center",
+                      fig.width = 6,
+                      fig.asp = 0.618,
+                      fig.show = "hold"
+)
+
+
